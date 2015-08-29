be47a242a099b367dc0dad37fab080625c1a024f
dadhaniya <dadhaniya@gmail.com>
2015-08-29 09:46:00 -0500

    Updates

--- index.js
+++ index.js
@@ -13,16 +13,17 @@ var hueip = '192.168.1.119';
 var hueusername = '';
 var hue = new HueApi(hueip,hueusername);
 
+//create array based on number of hue lights with empty values
+var light=[]; for (i = 1; i <= nhuelights; i++) {light[i] = '';}
 
-//Change Activity Function ID is activity ID from Harmony Hub, name is only for logging purposes
-function changeActivity(name,id){
+//Function to change activity, activity ID from Harmony Hub, name is only for logging purposes
+function changeActivity(name, id){
 	harmony(harmonyip)
 	.then(function(harmonyClient) {
 		winston.info('Starting ' + name + '...');
 		harmonyClient.startActivity(id).end();
 	});
-}
-
+};
 
 app.get('/', function (req, res) {
 	res.send('Hello World!');
@@ -38,44 +39,39 @@ app.get('/StopTV', function (req, res) {
 	res.sendStatus(200);
 });
 
-//Start TV
+//Harmony Hub Activities
 app.get('/WatchTV', function (req, res) { //Path set for activity in Amazon Echo Bridge Configurator
 	changeActivity('Watch TV','XXXXXXXX');
 	res.sendStatus(200);
 });
 
-//Start Apple TV
 app.get('/AppleTV', function (req, res) {
 	changeActivity('AppleTV','XXXXXXXX');
 	res.sendStatus(200);
 });
 
-//Start Firestick
 app.get('/Firestick', function (req, res) {
 	changeActivity('Firestick','XXXXXXXX');
 	res.sendStatus(200);
 });
 
-//Start Xbox One
 app.get('/Xbox', function (req, res) {
 	changeActivity('Xbox One','XXXXXXXX');
 	res.sendStatus(200);
 });
 
-//Start WiiU
 app.get('/WiiU', function (req, res) {
 	changeActivity('Wii U','XXXXXXXX');
 	res.sendStatus(200);
 });
 
-//Start Chromecast
 app.get('/Chromecast', function (req, res) {
 	changeActivity('Chromecast','XXXXXXXX');
 	res.sendStatus(200);
 });
 
 
-//Get list of activity ids
+//Get list of Harmony Hub activity ids
 app.get('/Activities', function (req, res) {
 	harmony(harmonyip)
 	.then(function(harmonyClient) {
@@ -90,73 +86,116 @@ app.get('/Activities', function (req, res) {
 	res.send('Check console for activity list');
 });
 
-//Get List of Hue Light
+//Get List of Hue Lights
 app.get('/ListLights', function (req, res) {
-	hue.lights()
-		.then(function(results) {
-			console.log(results);
-		})
-		.done();
+	hue.lights(function(err, result) {
+		if (err) throw err;
+			console.log(result);
+		});
 	res.sendStatus(200);
 });
 
 //Get List of Hue Scenes
 app.get('/ListScenes', function (req, res) {
-	hue.scenes()
-		.then(function(results) {
-			console.log(results);
-		})
-		.done();
+	hue.scenes(function(err, result) {
+		if (err) throw err;
+			console.log(result);
+		});
 	res.sendStatus(200);
 });
 
 //Get Current State of Lights
 app.get('/GetHueState', function (req, res) {
 	for (i = 1; i <= nhuelights; i++) {
-		hue.lightStatus(i)
-			.then(function(results) {
-				console.log(results);
-			})
-			.done();
+		hue.lightStatus(i, function(err, result) {
+			if (err) throw err;
+				console.log(result);
+			});
 	}
 	res.sendStatus(200);
 });
 
 //Start Sky Scene
 app.get('/SkyScene', function (req, res) {
-	hue.activateScene('62f8ac156-on-0') //Sky ID from Hue
-		.done();
+	hue.activateScene('62f8ac156-on-0', function(err,result) { //Sky ID from Hue
+		if (err) throw err;
+			result;
+		});
 	winston.info("Starting Sky Scene...");
 	res.sendStatus(200);
 });
 
+//Function to change color of lights based on light array variables
+function colorchange(name){
+	for (i = 1; i <= nhuelights; i++){
+		hue.setLightState(i, light[i], function(err, result) {
+			if (err) throw err;
+				result;
+			});
+		}
+	winston.info('Starting ' + name + ' Scene...');
+};
+
 //Start Nightlife Scene
 app.get('/NightlifeScene', function (req, res) {
-	hue.setLightState(1,{"on":true, "bri":254, "hue":47125, "sat":253, "effect":"none", "xy":[0.1684,0.0416], "ct":500}).then().done();
-	hue.setLightState(2,{"on":true, "bri":254, "hue":63494, "sat":253, "effect":"none", "xy":[0.6178,0.2911], "ct":500}).then().done();
-	hue.setLightState(3,{"on":true, "bri":254, "hue":48695, "sat":253, "effect":"none", "xy":[0.2115,0.0656], "ct":500}).then().done();
-	winston.info("Starting Nightlife Scene...");
-	res.sendStatus(200);
-});
-
-//Start Daredevil Scene
-app.get('/DaredevilScene', function (req, res) {
-	for (i = 1; i <= nhuelights; i++) {
-	hue.setLightState(i,{"on":true, "bri":254, "hue":65527, "sat":253, "effect":"none", "xy":[0.6736,0.3221], "ct":500,}).then().done();
-	}
-	winston.info("Starting Daredevil Scene...");
+	light[1]={"on":true, "bri":254, "hue":47125, "sat":253, "effect":"none", "xy":[0.1684,0.0416], "ct":500};
+	light[2]={"on":true, "bri":254, "hue":63494, "sat":253, "effect":"none", "xy":[0.6178,0.2911], "ct":500};
+	light[3]={"on":true, "bri":254, "hue":48695, "sat":253, "effect":"none", "xy":[0.2115,0.0656], "ct":500};
+	colorchange('Nightlife');
 	res.sendStatus(200);
 });
 
 //Start Aurora Scene
 app.get('/AuroraScene', function (req, res) {
-	hue.setLightState(1,{"on":true, "bri":85, "hue":15955, "sat":  79, "effect":"none", "xy":[0.4080,0.5170], "ct":307}).then().done();
-	hue.setLightState(2,{"on":true, "bri":85, "hue":15187, "sat": 129, "effect":"none", "xy":[0.3847,0.4707], "ct":353}).then().done();
-	hue.setLightState(3,{"on":true, "bri":51, "hue":15629, "sat": 102, "effect":"none", "xy":[0.4080,0.5170], "ct":325}).then().done();
-	winston.info("Starting Aurora Scene...");
+	light[1]={"on":true, "bri":85, "hue":15955, "sat":  79, "effect":"none", "xy":[0.4080,0.5170], "ct":307};
+	light[2]={"on":true, "bri":85, "hue":15187, "sat": 129, "effect":"none", "xy":[0.3847,0.4707], "ct":353};
+	light[3]={"on":true, "bri":51, "hue":15629, "sat": 102, "effect":"none", "xy":[0.4080,0.5170], "ct":325};
+	colorchange('Aurora');
 	res.sendStatus(200);
 });
 
+//Start Cinema Scene
+app.get('/CinemaScene', function (req, res) {
+	light[1]={"on":true, "bri":254, "hue":65527, "sat":253, "effect":"none", "xy":[0.6736,0.3221], "ct":500};
+	light[2]={"on":true, "bri":254, "hue":10660, "sat":252, "effect":"none", "xy":[0.5628,0.4034], "ct":500};
+	light[3]={"on":true, "bri":254, "hue":6396 , "sat":253, "effect":"none", "xy":[0.6077,0.3710], "ct":500};
+	colorchange('Cinema');
+	res.sendStatus(200);
+});
+
+//Same color scenes
+
+//Loop through all lights and change them to the same color, name is only for logging purposes
+function samecolor(name, lightcolor) {
+	for (i = 1; i <= nhuelights; i++) {
+		hue.setLightState(i, lightcolor, function(err, result) {
+			if (err) throw err;
+				result;
+			});
+		}
+	winston.info('Starting ' + name + ' Scene...');
+};
+
+//Start Daredevil Scene
+app.get('/DaredevilScene', function (req, res) {
+	var lightcolor = {"on":true, "bri":254, "hue":65527, "sat":253, "effect":"none", "xy":[0.6736,0.3221], "ct":500};
+	samecolor('Daredevil', lightcolor);
+	res.sendStatus(200);
+});
+
+//Start Blue Lights Scene
+app.get('/BlueLightsScene', function (req, res) {
+	var lightcolor = {"on":true, "bri":254, "hue":47125, "sat":253, "effect":"none", "xy":[0.1684,0.0416], "ct":500};
+	samecolor('Blue Lights', lightcolor);
+	res.sendStatus(200);
+});
+
+//Start Mood Scene
+app.get('/MoodScene', function (req, res) {
+	var lightcolor = {"on":true, "bri":84, "hue":3166, "sat":167, "effect":"none", "xy":[0.5525,0.3567], "ct":500};
+	samecolor('Mood', lightcolor);
+	res.sendStatus(200);
+});
 
 var server = app.listen(8088, function () {
 	var port = server.address().port;
