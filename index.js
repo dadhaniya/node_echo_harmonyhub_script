474da41974364fc8b5eb759342e29eb3e7f0e844
dadhaniya <dadhaniya@gmail.com>
2017-01-02 20:54:31 -0600

    Updated for 2 Rooms

--- index.js
+++ index.js
@@ -19,16 +19,15 @@ var winston = require('winston');
 	winston.add(winston.transports.File, {filename: 'history.json', json:true, timestamp:true, showLevel: true });
 
 //Hue variables
-var nhuelights = 3;
 var hueip = '192.168.1.118';
 var hueusername = '';
 var hueurl = 'http://'+hueip+'/api/'+hueusername;
+var lr_start = 1; //1st Living Room Light
+var br_start = 4; //1st Bedroom Light
 
-//create array based on number of hue lights with empty values
+//create empty light array
 var light = [];
-for (i = 1; i <= nhuelights; i++) {
-	light[i] = '';
-	}
+
 
 //Landing Page
 app.get('/', function (req, res) {
@@ -125,69 +124,79 @@ app.get('/ListScenes', function (req, res) {
 
 //Function to change color of lights based on light array variables
 function colorchange(name){
-	for (i = 1; i <= nhuelights; i++){
+	for (i = f_light; i <= f_light + 2; i++){
 		request({method:'PUT', url:hueurl + '/lights/' + i + '/state', json: light[i]}, function(error, request, body){});
 	}
 	winston.info('Starting ' + name + ' Scene...');
 }
 
+//Loop through all lights and change them to the same color, name is only for logging purposes
+function samecolor(name, lightcolor) {
+	for (i = f_light; i <= f_light + 2; i++) {
+		request({method:'PUT', url:hueurl + '/lights/' + i + '/state', json: lightcolor}, function(error, request, body){});
+	}
+	winston.info('Starting ' + name + ' Scene...');
+}
+
+
+//Living Room Scenes
+
 //Start Sky Scene
 app.get('/SkyScene', function (req, res) {
-	light[1] = {"on":true, "bri":254, "hue":34258, "sat":176, "effect":"none", "xy":[0.3327,0.3413], "ct":181};
-	light[2] = {"on":true, "bri":247, "hue":40891, "sat":252, "effect":"none", "xy":[0.2380,0.1797], "ct":500};
-	light[3] = {"on":true, "bri":254, "hue":34258, "sat":176, "effect":"none", "xy":[0.3327,0.3413], "ct":181};
+	f_light = lr_start;
+	light[f_light] 		= {"on":true, "bri":254, "hue":34258, "sat":176, "effect":"none", "xy":[0.3327,0.3413], "ct":181};
+	light[f_light + 1] 	= {"on":true, "bri":247, "hue":40891, "sat":252, "effect":"none", "xy":[0.2380,0.1797], "ct":500};
+	light[f_light + 2] 	= {"on":true, "bri":254, "hue":34258, "sat":176, "effect":"none", "xy":[0.3327,0.3413], "ct":181};
 	colorchange('Sky');
 	res.sendStatus(200);
 });
 
 //Start Nightlife Scene
 app.get('/NightlifeScene', function (req, res) {
-	light[1] = {"on":true, "bri":254, "hue":47125, "sat":253, "effect":"none", "xy":[0.1684,0.0416], "ct":500};
-	light[2] = {"on":true, "bri":254, "hue":63494, "sat":253, "effect":"none", "xy":[0.6178,0.2911], "ct":500};
-	light[3] = {"on":true, "bri":254, "hue":48695, "sat":253, "effect":"none", "xy":[0.2115,0.0656], "ct":500};
+	f_light = lr_start;
+	light[f_light] 		= {"on":true, "bri":254, "hue":47125, "sat":253, "effect":"none", "xy":[0.1684,0.0416], "ct":500};
+	light[f_light + 1] 	= {"on":true, "bri":254, "hue":63494, "sat":253, "effect":"none", "xy":[0.6178,0.2911], "ct":500};
+	light[f_light + 2] 	= {"on":true, "bri":254, "hue":48695, "sat":253, "effect":"none", "xy":[0.2115,0.0656], "ct":500};
 	colorchange('Nightlife');
 	res.sendStatus(200);
 });
 
 //Start Aurora Scene
 app.get('/AuroraScene', function (req, res) {
-	light[1] = {"on":true, "bri":85, "hue":15955, "sat":  79, "effect":"none", "xy":[0.4080,0.5170], "ct":307};
-	light[2] = {"on":true, "bri":85, "hue":15187, "sat": 129, "effect":"none", "xy":[0.3847,0.4707], "ct":353};
-	light[3] = {"on":true, "bri":51, "hue":15629, "sat": 102, "effect":"none", "xy":[0.4080,0.5170], "ct":325};
+	f_light = lr_start;
+	light[f_light] 		= {"on":true, "bri":85, "hue":15955, "sat":  79, "effect":"none", "xy":[0.4080,0.5170], "ct":307};
+	light[f_light + 1] 	= {"on":true, "bri":85, "hue":15187, "sat": 129, "effect":"none", "xy":[0.3847,0.4707], "ct":353};
+	light[f_light + 2] 	= {"on":true, "bri":51, "hue":15629, "sat": 102, "effect":"none", "xy":[0.4080,0.5170], "ct":325};
 	colorchange('Aurora');
 	res.sendStatus(200);
 });
 
 //Start Cinema Scene
 app.get('/CinemaScene', function (req, res) {
-	light[1] = {"on":true, "bri":254, "hue":65527, "sat":253, "effect":"none", "xy":[0.6736,0.3221], "ct":500};
-	light[2] = {"on":true, "bri":254, "hue":10660, "sat":252, "effect":"none", "xy":[0.5628,0.4034], "ct":500};
-	light[3] = {"on":true, "bri":254, "hue":6396 , "sat":253, "effect":"none", "xy":[0.6077,0.3710], "ct":500};
+	f_light = lr_start;
+	light[f_light] 		= {"on":true, "bri":254, "hue":65527, "sat":253, "effect":"none", "xy":[0.6736,0.3221], "ct":500};
+	light[f_light + 1] 	= {"on":true, "bri":254, "hue":10660, "sat":252, "effect":"none", "xy":[0.5628,0.4034], "ct":500};
+	light[f_light + 2] 	= {"on":true, "bri":254, "hue":6396 , "sat":253, "effect":"none", "xy":[0.6077,0.3710], "ct":500};
 	colorchange('Cinema');
 	res.sendStatus(200);
 });
 
 //Start Wave Scene
 app.get('/WaveScene', function (req, res) {
-	light[1] = {"on":true, "bri": 76, "hue":41977, "sat":252, "effect":"none", "xy":[0.2259,0.1557], "ct": 500};
-	light[2] = {"on":true, "bri": 72, "hue":41630, "sat":252, "effect":"none", "xy":[0.2298,0.1634], "ct": 500};
-	light[3] = {"on":true, "bri": 54, "hue":40760, "sat":253, "effect":"none", "xy":[0.2389,0.1818], "ct": 500};
+	f_light = lr_start;
+	light[f_light] 		= {"on":true, "bri": 76, "hue":41977, "sat":252, "effect":"none", "xy":[0.2259,0.1557], "ct": 500};
+	light[f_light + 1] 	= {"on":true, "bri": 72, "hue":41630, "sat":252, "effect":"none", "xy":[0.2298,0.1634], "ct": 500};
+	light[f_light + 2] 	= {"on":true, "bri": 54, "hue":40760, "sat":253, "effect":"none", "xy":[0.2389,0.1818], "ct": 500};
 	colorchange('Wave');
 	res.sendStatus(200);
 });
 
 //Same color scenes
 
-//Loop through all lights and change them to the same color, name is only for logging purposes
-function samecolor(name, lightcolor) {
-	for (i = 1; i <= nhuelights; i++) {
-		request({method:'PUT', url:hueurl + '/lights/' + i + '/state', json: lightcolor}, function(error, request, body){});
-	}
-	winston.info('Starting ' + name + ' Scene...');
-}
 
 //Start Reading Scene
 app.get('/ReadingScene', function (req, res) {
+	f_light = lr_start;
 	var lightcolor = {"on":true, "bri":254, "hue":14213, "sat":130, "effect":"none", "xy":[0.4556,0.4045], "ct":362};
 	samecolor('Reading', lightcolor);
 	res.sendStatus(200);
@@ -195,6 +204,7 @@ app.get('/ReadingScene', function (req, res) {
 
 //Start Daredevil Scene
 app.get('/DaredevilScene', function (req, res) {
+	f_light = lr_start;
 	var lightcolor = {"on":true, "bri":254, "hue":65527, "sat":253, "effect":"none", "xy":[0.6736,0.3221], "ct":500};
 	samecolor('Daredevil', lightcolor);
 	res.sendStatus(200);
@@ -202,15 +212,38 @@ app.get('/DaredevilScene', function (req, res) {
 
 //Start Blue Lights Scene
 app.get('/BlueLightsScene', function (req, res) {
+	f_light = lr_start;
 	var lightcolor = {"on":true, "bri":254, "hue":47125, "sat":253, "effect":"none", "xy":[0.1684,0.0416], "ct":500};
 	samecolor('Blue Lights', lightcolor);
 	res.sendStatus(200);
 });
 
+
+//Bedroom Scenes
+
+//Start Normal Scene
+app.get('/NormalScene', function (req, res) {
+	f_light = br_start;
+	var lightcolor = {"on":true, "bri":254, "hue":14213, "sat":130, "effect":"none", "xy":[0.4556,0.4045], "ct":362};
+	samecolor('Bed', lightcolor);
+	res.sendStatus(200);
+});
+
 //Start Mood Scene
 app.get('/MoodScene', function (req, res) {
+	f_light = br_start;
 	var lightcolor = {"on":true, "bri": 84, "hue": 3166, "sat":167, "effect":"none", "xy":[0.5525,0.3567], "ct":500};
 	samecolor('Mood', lightcolor);
+	res.sendStatus(200);
+});
+
+//Start Midnight Scene
+app.get('/MidnightScene', function (req, res) {
+	f_light = br_start;
+	light[f_light] 		= {"on":true, "bri":120, "hue":47125, "sat":253, "effect":"none", "xy":[0.1684,0.0416], "ct":500};
+	light[f_light + 1] 	= {"on":true, "bri":60, "hue":63494, "sat":253, "effect":"none", "xy":[0.6178,0.2911], "ct":500};
+	light[f_light + 2] 	= {"on":true, "bri":100, "hue":48695, "sat":253, "effect":"none", "xy":[0.2115,0.0656], "ct":500};
+	colorchange('Midnight');
 	res.sendStatus(200);
 });
 
